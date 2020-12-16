9d1a32873983ae6b4806187b15e77d85541fe116
FilipRomansci <filipromanski96@gmail.com>
2020-12-16 09:19:37 +0100

    chart plugin

--- pages/municipality/index.vue
+++ pages/municipality/index.vue
@@ -324,10 +324,6 @@
 
 <script>
 import firebase from '~/plugins/firebase'
-import Chartkick from 'vue-chartkick'
-import Chart from 'chart.js'
-import Vue from 'vue'
-Vue.use(Chartkick.use(Chart))
 
 export default {
   data() {
---
+++ plugins/chart.js
@@ -0,0 +1,5 @@
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+import Vue from 'vue'
+
+Vue.use(Chartkick.use(Chart))
