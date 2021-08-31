ffe55d7fc87e9275c406a88e6540aa40f1a3adb1
Dimitar Mitrevski <71487915+DimitarMitrevski@users.noreply.github.com>
2021-08-31 07:33:01 +0200

    Firebase config changed

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,15 +1,7 @@
 import firebase from 'firebase'
 
 var config = {
-  apiKey: 'AIzaSyCgMUTSfkEgNQcACdiVryCzGxxfT4tbPfQ',
-  authDomain: 'e-opstini.firebaseapp.com',
-  databaseURL:
-    'https://e-opstini-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'e-opstini',
-  storageBucket: 'e-opstini.appspot.com',
-  messagingSenderId: '156574750669',
-  appId: '1:156574750669:web:ae9fbb871117ea990f9684',
-  measurementId: 'G-2HH05KQS2X',
+ //Your config goes here
 }
 let app = null
 if (!firebase.apps.length) {
